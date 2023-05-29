c327140e213bdb08c741462da172bb690197e30e
Amritpal Singh <pritpal1637146@mail.com>
2023-05-29 09:35:30 +0530

    initilized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+
+function App(){
+  let title="React Course"
+  return(
+   <>
+    <div>Hello React world
+      <p>{title}</p>
+     <Header/>
+    </div>
+    </>
+  )
+}
+export default App
